54d4f37a1ec15f1cd0ccc9ffee7135da45102229
kkuusshh <sonhj98@naver.com>
2022-12-19 11:20:54 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.23.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Pulls the image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Create a container
+resource "docker_container" "son" {
+  image = docker_image.nginx.latest
+  name  = "son"
+}
